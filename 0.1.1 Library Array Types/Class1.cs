676a691b722bf97c36e42b1d84951e37b5543b31
Juan Beltrame <juan_beltrame@hotmail.com>
2024-12-27 06:01:05 -0300

    Arrray Update Files

---
+++ 0.1.1 Library Array Types/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace _0._1._1_Library_Array_Types
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ 0.2.1 Library Arrays Operations/Read.cs
@@ -0,0 +1,16 @@
+﻿namespace _0._2._1_Arrays_Operations
+{
+    public class Read
+    {
+        int[] numeros;
+
+        public Read() => numeros = [1,2,3,4,5,6,7,8,9];
+
+        public void ReadArray()
+        {
+            for (int i = 0; i < numeros.Length; i++) 
+                Console.WriteLine(numeros[i]);
+        }
+         
+    }
+}
