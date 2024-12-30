a2b2a0cc534dcd261e4afc24af0a1fc9656ca6b2
Juan Beltrame <juan_beltrame@hotmail.com>
2024-12-30 14:42:08 -0300

    Update

---
+++ 0.2.1 Library Arrays Operations/Write.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._2._1_Arrays_Operations
+{
+    public class Write
+    {
+        public static void WriteArray(int[] array, int position, int number)
+        {
+            int size = array.Length;
+            if (position < size)
+            {
+                array[position] = number;
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
