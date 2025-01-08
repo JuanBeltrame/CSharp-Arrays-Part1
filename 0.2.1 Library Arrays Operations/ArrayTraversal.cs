89eb20567a3f17507278538d48f63c5f3339c2dc
Juan Beltrame <juan_beltrame@hotmail.com>
2025-01-08 05:47:52 -0300

    Update

---
+++ 0.2.1 Library Arrays Operations/ArrayTraversal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._2._1_Arrays_Operations
+{
+    public class ArrayTraversal
+    {
+        public static void TraverseAndReadArray(int[] nums)
+        {
+            Console.Write($"The Elements are: ");
+            for (int i = 0; i < nums.Length; i++)
+                Console.Write($" {i}");
+        }
+    }
+}
--- 0.2.2 View Arrays Operations/Program.cs
+++ 0.2.2 View Arrays Operations/Program.cs
@@ -3,8 +3,8 @@ using _0._2._1_Arrays_Operations;
 
 //--------Read-------- 
 int[] array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-//Read.ReadArray(array);
 
+//AccessElementsbyIndex.ReadElementbyIndex(array);
+//Console.WriteLine(AccessElementsbyIndex.WriteElementbyIndex(array));
 
-AccessElementsbyIndex.ReadElementbyIndex(array);
-Console.WriteLine(AccessElementsbyIndex.WriteElementbyIndex(array));
+ArrayTraversal.TraverseAndReadArray(array);
