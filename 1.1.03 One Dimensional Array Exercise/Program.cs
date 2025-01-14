9d6bdcf45c193ed4cc833210997e59bf990634cd
Juan Beltrame <juan_beltrame@hotmail.com>
2025-01-14 14:35:51 -0300

    New Exercise added

--- 1.1.03 One Dimensional Array Exercise/Program.cs
+++ 1.1.03 One Dimensional Array Exercise/Program.cs
@@ -13,19 +13,48 @@ luego obtener la suma de los valores positivos dentro del conjunto.
 
 static void LoadArray(int[] array)
 {
-    Random rn = new(); 
+    Random rn = new();
     int minValue = -10;
     int maxValue = 11;
 
-    for (int i = 0; i < array.Length ; i++)
+    int capacity = array.Length;
+    for (int i = 0; i < capacity; i++)
         array[i] = rn.Next(minValue, maxValue);
 }
 
-static void PrintArray(int[] array)
+static void PrintIntialValues(int[] array)
 {
+    int capacity = array.Length;
     Console.Write($"Initial Values: ");
-    for (int i = 0; i < array.Length ; i++)
-        Console.Write($" {array[i]}");
+    for (int i = 0; i < capacity; i++)
+        Console.Write($"{array[i]} ");
+    System.Console.WriteLine();
+}
+
+static void PrintPositivesValues(int[] array)
+{
+    int capacity = array.Length;
+    int negativeValue = 0;
+    System.Console.Write($"Positives Values are: ");
+    for (int i = 0; i < capacity; i++)
+    {
+        if (array[i] > negativeValue)
+            System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+static int SumPositivesValues(int[] array)
+{
+    int capacity = array.Length;
+    int negativeValue = 0;
+    int acumulator = default;
+    for (int i = 0; i < capacity; i++)
+    {
+        if (array[i] > negativeValue)
+            acumulator += array[i];
+    }
+    return acumulator;
 }
 
 
@@ -33,7 +62,8 @@ int size = 25;
 int[] array = new int[size];
 
 LoadArray(array);
-PrintArray(array);
-
+PrintIntialValues(array);
+PrintPositivesValues(array);
+Console.Write($"Sum of Positives Values: {SumPositivesValues(array)}");
 
 
