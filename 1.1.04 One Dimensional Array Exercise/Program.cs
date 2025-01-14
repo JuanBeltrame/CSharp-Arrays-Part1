f0eaa28bd5c59024be72f1fc056e643ce77c926b
Juan Beltrame <juan_beltrame@hotmail.com>
2025-01-14 15:12:30 -0300

    New exercise added.

---
+++ 1.1.04 One Dimensional Array Exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+One-Dimensional Array 
+----------------------------------------------------------------------
+English:
+Enter 30 elements of type char in an array
+and then display only the elements that are different from ' * ', 
+indicating the position that element occupies in the array. 
+
+Spanish: 
+Ingresar 30 elementos de tipo char en un arreglo
+y luego mostrar sólo los elementos que sean distintos de ‘ * ‘, 
+indicando la posición que dicho elemento ocupa en el arreglo.
+----------------------------------------------------------------------
+*/
+
+
+
+char[] array = new char[30];
+
+void PrintIntialValues(char[] array)
+{
+    Random randomChar = new();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = (char)randomChar.Next(32,127);
+}
+
