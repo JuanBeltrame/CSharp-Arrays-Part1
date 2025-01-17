91fb5cfda669b7fd23e1d1cdcbf32adf27ba930c
Juan Beltrame <juan_beltrame@hotmail.com>
2025-01-17 15:35:04 -0300

    Exercise Description added

---
+++ 1.1.05 One Dimensional Array Exercise/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+One-Dimensional Array 
+----------------------------------------------------------------------
+English:
+Enter integers into an array A of 10 elements 
+and enter integers in another array B of equal dimension (use procedure LOAD with parameters).
+
+Obtain, an array C, being C = A + B. (each element of C is obtained as the sum of the homologous elements of A and B).
+a. Show A, B and C, one below the other.
+b. Show A, B, and C, side by side.
+
+Spanish: 
+Ingresar números enteros en un arreglo A de 10 elementos 
+e ingresar números enteros en otro arreglo B de igual dimensión (utilizar procedure CARGA con parámetros).
+
+Obtener, un arreglo C, siendo C = A + B. (cada elemento de C se obtiene como la suma de los elementos homólogos de A y B).
+a. Mostrar A, B y C, uno debajo del otro.
+b. Mostrar A, B y C, uno al lado del otro.
+----------------------------------------------------------------------
+*/
