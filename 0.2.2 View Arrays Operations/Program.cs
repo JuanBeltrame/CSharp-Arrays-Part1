d8b2c25963fc00d356629b6d0217125d810546d5
Juan Beltrame <juan_beltrame@hotmail.com>
2025-01-20 04:03:26 -0300

    Update

--- 0.2.2 View Arrays Operations/Program.cs
+++ 0.2.2 View Arrays Operations/Program.cs
@@ -1,22 +1,16 @@
-﻿using _0._2._1_Arrays_Operations;
-using System;
+﻿
+using _0._2._1_Arrays_Operations;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        //--------Read-------- 
-        int[] readArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        //--------Write-------- 
-        int[] writeArray = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+//--------Read-------- 
+int[] readArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//--------Write-------- 
+int[] writeArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-        //AccessElementsbyIndex.ReadElementbyIndex(array);
-        //Console.WriteLine(AccessElementsbyIndex.WriteElementbyIndex(array));
+//AccessElementsbyIndex.ReadElementbyIndex(array);
+//Console.WriteLine(AccessElementsbyIndex.WriteElementbyIndex(array));
 
-        ArrayTraversal.TraverseAndReadArray(readArray);
-        Console.WriteLine();
-        ArrayTraversal.TraverseAndWriteArray(readArray, writeArray);
-        Console.WriteLine();
-        Console.WriteLine($"Size: {ArraySize.CalculateSize(readArray)}");
-    }
-}
+ArrayTraversal.TraverseAndReadArray(readArray);
+Console.WriteLine();
+ArrayTraversal.TraverseAndWriteArray(readArray, writeArray);
+Console.WriteLine();
+Console.WriteLine($"Size: {ArraySize.CalculateSize(readArray)}");
