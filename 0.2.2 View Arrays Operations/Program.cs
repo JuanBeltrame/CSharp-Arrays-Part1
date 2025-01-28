89bd8321590023e76d01099749c17d99f990a09d
Juan Beltrame <juan_beltrame@hotmail.com>
2025-01-28 02:55:06 -0300

    .

--- 0.2.2 View Arrays Operations/Program.cs
+++ 0.2.2 View Arrays Operations/Program.cs
@@ -6,9 +6,9 @@ int[] readArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 //--------Write-------- 
 int[] writeArray = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-//AccessElementsbyIndex.ReadElementbyIndex(array);
-//Console.WriteLine(AccessElementsbyIndex.WriteElementbyIndex(array));
-
+AccessElementsbyIndex.ReadElementbyIndex(readArray);
+Console.WriteLine(AccessElementsbyIndex.WriteElementbyIndex(writeArray));
+Console.WriteLine();
 ArrayTraversal.TraverseAndReadArray(readArray);
 Console.WriteLine();
 ArrayTraversal.TraverseAndWriteArray(readArray, writeArray);
