929fae38f307a9c43247e3ae7a16f6293ebae1c8
Juan Beltrame <juan_beltrame@hotmail.com>
2025-02-20 04:01:26 -0300

    feat: add 1110 One Dimensional Array Exercise project
    - Added a new project "1110OneDimensionalArrayExercise" to the
      solution file `Arrays-Part1.sln` with configuration settings for
      Debug and Release modes.
    
    - Created the project file `1110OneDimensionalArrayExercise.csproj`
      using the new SDK format, specifying properties like
      `OutputType`, `TargetFramework`, `RootNamespace`, and enabling
      `ImplicitUsings` and `Nullable` features.
    
    - Introduced a new `Program.cs` file that implements a console
      application to prompt the user for a word, check if it is a
      palindrome, and display the word along with its length. Comments
      are provided in both English and Spanish.
    
    - Implemented palindrome checking logic, with some issues in the
      while loop that need to be addressed.

---
+++ 1.1.10 One Dimensional Array Exercise/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+One-Dimensional Array
+----------------------------------------------------------------------
+English: 
+Enter a word with a maximum of ten letters. 
+Inform if it is a palindrome (equivalent to a capicua if it is numeric). For example: NEUQUEN. 
+The end of the word is indicated with an ‘*’ if it has less than ten letters.
+
+Spanish: 
+Ingresar una palabra que como máximo tenga diez letras. 
+Informar si es un palíndromo (equivalente a capicúa si fuese numérico). Por ejemplo: NEUQUEN. 
+Se indica con un “*” el fin de la palabra si tiene menos de diez letras.
+----------------------------------------------------------------------
+*/
+
+Console.Title = "One-Dimensional Array";
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+string word;
+
+Console.Write("Ingresar una Palabra: ");
+word = Console.ReadLine()!;
+Console.WriteLine($"La Palabra ingresada fue: {word}");
+
+char[] letters = word.ToCharArray();
+int longitudCadena = word.Length;
+Console.WriteLine($"{word} tiene {longitudCadena} letras");
+
+int posInicial = default;
+int posFinal = word.Length;
+
+
+while ((letters[posInicial] == letters[posFinal -1]) || (posInicial == 10) || letters[posInicial] == '*' )
+{
+    
+    posInicial = +1;
+    posFinal = -1;
+
+}
+Console.WriteLine($"{word} No es Palindromo");
+
+
+
+for (int i = 0; i < letters.Length; i++)
+{
+    Console.Write($"{letters[i]} ");
+}
+
+
+
+
